b4cb771ac7b2f7847ab9d8322c123914881e9e20
Michael Crismali <michael.crismali@gmail.com>
2014-01-25 17:05:12 -0600

    added a bunch of #on_send stuff

--- lib/slide/rewriter.rb
+++ lib/slide/rewriter.rb
@@ -1,13 +1,27 @@
 class Slide::Rewriter < Parser::Rewriter
 
-  OPEN_PAREN = "("
   CLOSE_PAREN = ")"
+  ELSE_IF = "else if"
+  ELSIF = "elsif"
+  OPEN_PAREN = "("
   QUESTION_MARK = "?"
+  SPACE = " "
+
+  BRACKET_WRAP = /\A\[.*\]\z/
 
   def on_if(node)
     super
+    keyword = node.loc.keyword
     conditional_expression = node.children.first.loc.expression
+    replace keyword, ELSE_IF if keyword.source == ELSIF
     wrap_in_existential_operator(conditional_expression)
+  end
+
+  def on_send(node)
+    super
+    return if node.loc.selector.source.match BRACKET_WRAP
+    insert_after node.loc.selector.end, OPEN_PAREN
+    insert_after node.loc.expression.end, CLOSE_PAREN
   end
 
   private
--- spec/rewriter_spec.rb
+++ spec/rewriter_spec.rb
@@ -9,30 +9,121 @@ describe Slide::Rewriter do
 
   describe "#on_if" do
 
-    let(:nested_if) do
-      %|
-        if (x = 5 if y == 3)
-          puts "true"
-        else
-          puts "false"
+    it "converts elsif to 'else if'" do
+      buffer.source = <<EOF
+        if self
+          true
+        elsif 5
+          true
         end
-      |
-    end
-
-    let(:expected) do
-      %|
-        if ((x = 5 if (y == 3)?))?
-          puts "true"
-        else
-          puts "false"
+EOF
+      expected = <<EOF
+        if (self)?
+          true
+        else if (5)?
+          true
         end
-      |
-    end
-
-    it "wraps the condition in the existential operator ('?')" do
-      buffer.source = nested_if
+EOF
       results = rewriter.rewrite(buffer, ast)
       expect(results).to eq(expected)
     end
+
+    context "wrapping conditions in existential operator (?)" do
+      let(:nested_if) do
+        %|
+          if (5 if nil)
+            true
+          else
+            false
+          end
+        |
+      end
+
+      let(:expected) do
+        %|
+          if ((5 if (nil)?))?
+            true
+          else
+            false
+          end
+        |
+      end
+
+      it "when an if" do
+        buffer.source = nested_if
+        results = rewriter.rewrite(buffer, ast)
+        expect(results).to eq(expected)
+      end
+
+      it "when an unless" do
+        buffer.source = "5 unless nil"
+        results = rewriter.rewrite(buffer, ast)
+        expect(results).to eq("5 unless (nil)?")
+      end
+
+      it "when an elsif" do
+        buffer.source = <<EOF
+          if self
+            true
+          elsif 5
+            true
+          end
+EOF
+        expected = <<EOF
+          if (self)?
+            true
+          else if (5)?
+            true
+          end
+EOF
+        results = rewriter.rewrite(buffer, ast)
+        expect(results).to eq(expected)
+      end
+    end
+  end
+
+  describe "BRACKET_WRAP" do
+
+    let(:bracket_wrap) { Slide::Rewriter::BRACKET_WRAP }
+
+    it "matches strings that start and end with curly brackets" do
+      expect("[]").to match(bracket_wrap)
+      expect("[5]").to match(bracket_wrap)
+      expect("[]").to match(bracket_wrap)
+      expect(" []").to_not match(bracket_wrap)
+      expect("]").to_not match(bracket_wrap)
+      expect("[").to_not match(bracket_wrap)
+    end
+  end
+
+
+  describe "#on_send" do
+
+    it "puts parentheses after method calls with no arguments" do
+      buffer.source = "self.call"
+      results = rewriter.rewrite(buffer, ast)
+      expect(results).to eq("self.call()")
+    end
+
+    it "puts parentheses after implicit method calls on self" do
+      buffer.source = "invoke"
+      results = rewriter.rewrite(buffer, ast)
+      expect(results).to eq("invoke()")
+    end
+
+    it "puts parentheses around the method's arguments" do
+      buffer.source = "invoke arg_1, arg_2"
+      results = rewriter.rewrite(buffer, ast)
+      expect(results).to eq("invoke( arg_1(), arg_2())")
+    end
+
+    it "doesn't invoke bracket ('[]') methods" do
+      code = "self[5]"
+      buffer.source = code
+      results = rewriter.rewrite(buffer, ast)
+      expect(results).to eq(code)
+    end
+
+    it "puts parentheses around block arguments"
   end
 end
